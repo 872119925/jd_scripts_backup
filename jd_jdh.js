6e33d8c78692886088203fab81d6f575c967b6da
Kinson yan <kinson.yan@outlook.com>
2020-12-16 19:07:05 +0800

    update newShareCodes

--- jd_jdh.js
+++ jd_jdh.js
@@ -39,8 +39,12 @@ if ($.isNode()) {
   cookiesArr.reverse();
 }
 const JD_API_HOST = 'https://api.m.jd.com/api';
+$.newShareCodes = ['1d5267ba926ee7bc8b525c11b61085af@880f3003021f56bf2a8d77364009975c@f532fddf17661aa160c230f406a4a5f6',
+                  '9dbf38390cccb579123926bc4a0012e9@880f3003021f56bf2a8d77364009975c@f532fddf17661aa160c230f406a4a5f6',
+                   '9dbf38390cccb579123926bc4a0012e9@1d5267ba926ee7bc8b525c11b61085af@f532fddf17661aa160c230f406a4a5f6',
+                   '9dbf38390cccb579123926bc4a0012e9@1d5267ba926ee7bc8b525c11b61085af@880f3003021f56bf2a8d77364009975c'
+                  ];
 !(async () => {
-  $.newShareCodes = [];
   if (!cookiesArr[0]) {
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/', {"open-url": "https://bean.m.jd.com/"});
     return;
